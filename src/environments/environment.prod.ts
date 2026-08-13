e293a0e6af34ae77348ae937411b77941b0f0dc0
juanPablomtz <juan.pablomati23@gmail.com>
2026-08-13 12:38:05 -0600

    url de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://promoting-decided-encounter-software.trycloudflare.com',
+  socketUrl: 'https://promoting-decided-encounter-software.trycloudflare.com'
 };
